5a6fe250472b8fc6459a2380d4f2110c34d41b97
Umar Tung <utung@brocksolutions.com>
2019-07-04 13:51:02 -0700

    Initialize basic classes

---
+++ ClassLibrary1/Enums/EquipmentType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NinjaDomain.Classes
+{
+    public enum EquipmentType
+    {
+        Tool = 1,
+        Weapon = 2,
+        Outwear = 3,
+    }
+}
---
+++ ClassLibrary1/Enums/NinjaType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaDomain.Classes
+{
+    public enum NinjaType
+    {
+        Shinobi = 1,
+        Kunoichi = 2,
+    }
+}
